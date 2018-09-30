4e092122355f4f157297ba573f22ed0ce606fade
lelelelion <MiaoLe@mail.dlut.edu.cn>
2018-09-30 16:23:16 +0800

    添加退票接口,修复订票接口事务回滚失败的bug

--- controller/order-controller.js
+++ controller/order-controller.js
@@ -24,64 +24,67 @@ const {
  * @returns {Promise<void>}
  */
 const generateOrder = async ctx => {
+    let user = await checkUser(ctx);
+    if (!user || !checkPostParams(ctx, ['ticketId', 'passengers', 'contactName', 'phone']))
+        return;
+    let {body} = ctx.request;
+    if (!body.passengers || !body.passengers.length || body.passengers.length <= 0) {
+        ctx.easyResponse.error('please add passengers');
+        return;
+    }
+
+    let ticket = await Ticket.findById(body.ticketId);
+    // 检查票数是否足够
+    if (ticket.standbyTicket < body.passengers.length) {
+        ctx.easyResponse.error('ticket is not enough for ' + body.passengers.length + ' people');
+        return;
+    }
+    //检查余额是否足够
+    if (!ticket) {
+        ctx.easyResponse.error('ticket not exists');
+        return;
+    }
+    let cost = parseInt((ticket.price * ticket.discount)) * body.passengers.length;
+    if (cost > user.money) {
+        ctx.easyResponse.error('Sorry, your credit is running low');
+        return;
+    }
+
+    body.id = undefined;
+    body.uid = undefined;
+    body.ticketId = undefined;
+
     /**
      * 开启一个事务，失败则回滚
      */
     await sequelize.transaction(async t => {
-        let user = await checkUser(ctx);
-        if (!user || !checkPostParams(ctx, ['ticketId', 'passengers', 'contactName', 'phone']))
-            return;
-        let {body} = ctx.request;
-        if (!body.passengers || !body.passengers.length || body.passengers.length <= 0) {
-            ctx.easyResponse.error('please add passengers');
-            return;
-        }
 
-        console.log(body)
-        let ticket = await Ticket.findById(body.ticketId);
-        // 检查票数是否足够
-        if (ticket.standbyTicket < body.passengers.length) {
-            ctx.easyResponse.error('ticket is not enough for ' + body.passengers.length + ' people');
-            return;
-        }
-        //检查余额是否足够
-        if (!ticket) {
-            ctx.easyResponse.error('ticket not exists');
-            return;
-        }
-        let cost = parseInt((ticket.price * ticket.discount)) * body.passengers.length;
-        if (cost > user.money) {
-            ctx.easyResponse.error('Sorry, your credit is running low');
-            return;
-        }
-
-        body.id = undefined;
-        body.uid = undefined;
-        body.ticketId = undefined;
-        let order = await Order.create({
+        return Order.create({
             ...body,
             cost: cost
-        });
-        if (!order)
-            ctx.easyResponse.error('crate oder fail');
-        console.log(JSON.stringify(user));
-        await user.addOrder(order);
-        await ticket.addOrder(order);
+        }, {transaction: t})
+            .then(async order => {
+                if (!order) {
+                    ctx.easyResponse.error('crate oder fail');
+                    return;
+                }
+                await user.addOrder(order, {transaction: t});
+                await ticket.addOrder(order, {transaction: t});
+                await asyncForEach(body.passengers, async value => {
+                    let passenger = await PassengerContact.findById(value);
+                    if (!passenger) {
+                        ctx.easyResponse.error('passenger contact not exists for: ' + value);
+                        return;
+                    }
+                    await passenger.addOrders(order, {transaction: t});
+                });
 
-        await asyncForEach(body.passengers, async value => {
-            let passenger = await PassengerContact.findById(value);
-            if (!passenger) {
-                ctx.easyResponse.error('passenger contact not exists for: ' + value);
-                return;
-            }
-            await passenger.addOrders(order);
-        });
-
-        user.money = user.money - cost;
-        await user.save();
-        ticket.standbyTicket = ticket.standbyTicket - 1;
-        await ticket.save();
-        ctx.easyResponse.success(order, 'createOrderSuccess');
+                user.money = user.money - cost;
+                await user.save({transaction: t});
+                ticket.standbyTicket = ticket.standbyTicket - 1;
+                await ticket.save({transaction: t});
+                ctx.easyResponse.success(order, 'createOrderSuccess');
+            });
     });
 };
 
@@ -162,9 +165,51 @@ const deleteOrder = async ctx => {
     ctx.easyResponse.success({affectedCount});
 };
 
+/**
+ * 退票
+ * @param ctx
+ * @returns {Promise<void>}
+ */
+const returnTicket = async ctx => {
+    let user = await checkUser(ctx);
+    if (!user || !checkPostParams(ctx, ['orderId']))
+        return;
+    let order = await Order.findOne({
+        where: {
+            id: ctx.request.body.orderId,
+        },
+        include: ['passengers', User, {
+            model: Ticket,
+            include: [Flight]
+        },]
+    });
+    if (!order) {
+        ctx.easyResponse.error("订单不存在");
+        return;
+    }
+    if (order.status !== 0) {     //订单已经完成,无法退票
+        ctx.easyResponse.error("订单已经完成,无法退票");
+        return;
+    }
+
+    // 开启一个事务执行退票操作
+    await sequelize.transaction(async t => {
+        //置订单为退票状态
+        order.status = 2;
+        user.money += order.cost;
+        console.log(user);
+        return order.save({transaction: t})
+            .then(order => {
+                return user.save({transaction: t})
+            })
+    });
+    ctx.easyResponse.success("", "退票成功")
+};
+
 module.exports = {
     'POST /generateOrder': generateOrder,
     'GET /getOrderDetail': getOrderDetail,
     'GET /getOrders': getOrders,
     'POST /deleteOrder': deleteOrder,
+    'POST /returnTicket': returnTicket,
 };
